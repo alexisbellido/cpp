59d8b8a45f787df9b5d976d4895f72558348fc98
Alexis Bellido <alexis@ventanazul.com>
2016-01-06 20:55:29 -0500

    acc input

--- acc/small.cpp
+++ acc/small.cpp
@@ -10,5 +10,7 @@ int main()
 {
     std::cout << "Hello there" << std::endl;
     std::cout << sum(2, 3) << std::endl;
+    std::cout << "A\tB\tC\tD\tE\n";
+    std::cout << "12\t23\t34\t21\t14\n";
     return 0;
 }
