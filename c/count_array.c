a76edadd19ecedd8eb5669f509da5cf3814f1518
Alexis Bellido <alexis@ventanazul.com>
2017-08-11 21:35:58 -0400

    power function

---
+++ c/count_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/* Count digits, white space, others */
+/* For OSX you need to press Return and then CTRL+D in an empty line to signal EOF */
+
+int main() {
+
+    int c, i, nwhite, nother;
+    int ndigit[10];
+
+    nwhite = nother = 0;
+    for (i = 0; i < 10; ++i) {
+        printf("i = %d\n", i);
+        ndigit[i] = 0;
+    }
+
+
+    while ((c = getchar()) != EOF) {
+        if (c >= '0' && c <= '9') {
+            ++ndigit[c-'0'];
+        } else if (c == ' ' || c == '\n' || c == '\t') {
+            ++nwhite;
+        } else {
+            ++nother;
+        }
+    }
+
+    printf("digits =");
+    for (i = 0; i < 10; ++i) {
+        printf(" %d", ndigit[i]);
+    }
+
+    printf(", white space = %d, other = %d\n", nwhite, nother);
+
+}
